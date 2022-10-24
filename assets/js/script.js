13d4366f7ecfe7e8b40b7b7f92c7adf837168cbb
Leetzner <ognill@hotmail.com>
2022-10-24 09:58:32 +0000

    Made the computer randomly select between words

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//** This function makes the computer randomly selects between `rock`
+ //`paper``scissor` `lizard``spock`**
 
+function getComputerChoice(){
+    const rpslsChoice = ['Rock', 'Paper', 'Scissor', 'Lizard', 'Spock']
+    const randomNumber = Math.floor(Math.random()* 5)
+    return rpslsChoice[randomNumber]
+}
 
 
